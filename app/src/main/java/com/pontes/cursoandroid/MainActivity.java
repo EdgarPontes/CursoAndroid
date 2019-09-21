f56be76fd88d05cef1da239929a341d5e85eeccf
Edgar Pontes <edgarponte@gmail.com>
2019-09-20 22:22:10 -0300

    Criando layout e botoẽs

--- app/src/main/java/com/pontes/cursoandroid/MainActivity.java
+++ app/src/main/java/com/pontes/cursoandroid/MainActivity.java
@@ -2,13 +2,63 @@ package com.pontes.cursoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton fabMain, fabExpenses, fabEarnings;
+    private boolean fabsHide = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainer, new MainFragment()).commit();
+
+        fabMain = findViewById(R.id.fabMain);
+
+        fabMain.setOnClickListener(this);
+
+        fabEarnings = findViewById(R.id.fabEarnings);
+        fabExpenses = findViewById(R.id.fabExpenses);
+
+        //ButterKnife.bind(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.fabMain){
+            showHideFabs();
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void showHideFabs(){
+
+        AlphaAnimation fabAnimation = new AlphaAnimation(0,1);
+        fabAnimation.setDuration(1000);
+        fabAnimation.setStartOffset(100);
+
+        if (fabsHide) {
+            fabExpenses.setVisibility(View.VISIBLE);
+            fabEarnings.setVisibility(View.VISIBLE);
+            fabExpenses.setAnimation(fabAnimation);
+            fabEarnings.setAnimation(fabAnimation);
+        }else{
+            fabExpenses.clearAnimation();
+            fabEarnings.clearAnimation();
+            fabExpenses.setVisibility(View.INVISIBLE);
+            fabEarnings.setVisibility(View.INVISIBLE);
+        }
+
+        fabsHide = !fabsHide;
     }
 }
